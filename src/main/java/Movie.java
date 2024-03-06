53f414559bb0ea9c40c00bcebcccfe7035878877
דניאל <160715078+TheDanielTp@users.noreply.github.com>
2024-03-06 10:07:47 +0330

    Update Movie Class via Upload
    completed get imdb votes function
    new constructor added for strings array
    completed get ratings function
    some minor edits to other functions
    completed get actors list function

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -4,25 +4,34 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.io.BufferedReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class Movie
 {
-    public static final String API_KEY = "520cf7ab";
+    public static final String API_KEY = "520cf7ab"; //api key received from omdbapi
     int ImdbVotes;
     ArrayList<String> actorsList;
     String rating;
+    String[] ratings;
 
-    public Movie(ArrayList<String> actorsList, String rating, int ImdbVotes){
+    public Movie(ArrayList<String> actorsList, String rating, int ImdbVotes) { //constructor function for initializing the object
         this.actorsList = actorsList;
-        this.rating = rating;
+        this.rating = rating; //inputs a single string
         this.ImdbVotes = ImdbVotes;
     }
 
-    @SuppressWarnings("deprecation")
+    public Movie(ArrayList<String> actorsList, String[] ratings, int ImdbVotes) { //constructor function for finalizing the object
+        this.actorsList = actorsList;
+        this.ratings = ratings; //inputs an array of strings
+        this.ImdbVotes = ImdbVotes;
+    }
+
     /**
-     * Retrieves data for the specified movie.
-     * @param title the name of the title for which MovieData should be retrieved
-     * @return a string representation of the MovieData, or null if an error occurred
+     * retrieves data for the specified movie.
+     * @param title the name of the title for which moviedata should be retrieved
+     * @return a string representation of the moviedata, or null if an error occurred
     */
 
     public String getMovieData(String title) throws IOException
@@ -44,9 +53,8 @@ public class Movie
         reader.close(); //closes the reader once all lines have been read
 
         String response = stringBuilder.toString(); //converts the stringbuilder to a string and assigns it to response
-        System.out.println(response);
 
-        if (response.contains("False")) { // Check if the response contains an error message
+        if (response.contains("False")) { //check if the response contains an error message
             return "Error: Movie not found.";
         }
         return response;
@@ -54,20 +62,63 @@ public class Movie
 
     public int getImdbVotesViaApi(String moviesInfoJson)
     {
-        //TODO --> (This function must change and return the "ImdbVotes" as an Integer)
-        //NOTICE :: you are not permitted to convert this function to return a String instead of an int !!!
-        int ImdbVotes = 0;
-        return ImdbVotes;
+        JSONObject moviesJsonObject = new JSONObject(moviesInfoJson); //parses the json string into a json object
+
+        if (moviesJsonObject.has("imdbVotes")) { //checks if the imdbvotes exists in the json object
+            String imdbVotesStr = moviesJsonObject.getString("imdbVotes"); //creates a string and gets the value of imdbvotes
+
+            imdbVotesStr = imdbVotesStr.replaceAll("[^\\d]", ""); //removes characters which are not numbers
+
+            try { //parse the imdbvotes string to an integer
+                int imdbVotesInt = Integer.parseInt(imdbVotesStr);
+                return imdbVotesInt;
+            }
+            catch (NumberFormatException e) { //handles any possible errors
+                System.err.println("Error parsing IMDB votes: " + e.getMessage());
+            }
+        }
+        return 0; //returns 0 if imdbvotes field is not found or cannot be parsed
     }
 
-    public String getRatingViaApi(String moviesInfoJson){
-        //TODO --> (This function must return the rating in the "Ratings" part
-        //TODO --> where the source is "Internet Movie Database")  -->
-        String rating = "";
-        return rating;
+
+    public String getRatingViaApi(String moviesInfoJson)
+    {
+        JSONObject ratingsJsonObject = new JSONObject(moviesInfoJson); //parses the json string into a json object
+
+        JSONArray ratingsArray = ratingsJsonObject.getJSONArray("Ratings"); //extract ratings jsonarray from the jsonobject
+        String[] ratings = new String[ratingsArray.length()]; //array to hold individual ratings
+
+        for (int i = 0; i < ratingsArray.length(); i++) { //extract each rating from the jsonarray and put it into the ratings array
+            JSONObject ratingObject = ratingsArray.getJSONObject(i); //creates a jsonobject for each individual object of the array
+
+            String source = ratingObject.getString("Source"); //stores the name of the source
+            String value = ratingObject.getString("Value"); //stores the score which the movie got form the source
+
+            ratings[i] = source + ": " + value; //combines the strings to define each array
+        }
+        return Arrays.toString(ratings);
     }
 
-    public void getActorListViaApi(String movieInfoJson){
-        //TODO --> (This function must return the "Actors" in actorsList)
+    public String getActorListViaApi(String movieInfoJson)
+    {
+        JSONObject actorsJsonObject = new JSONObject(movieInfoJson); //parses the json string into a json object
+        String[] actorsListArray = actorsList.toArray(new String[actorsList.size()]); //string array to contain the final anwer
+
+        if (actorsJsonObject.has("Actors")) { //checks if the actors field exists in the json data
+            String actorsString = actorsJsonObject.getString("Actors"); //extracts the actors string from the json data
+
+            String[] actorsArray = actorsString.split(", "); //splits the actors string into individual actor names
+
+            for (String actor : actorsArray) { //adds each actor name to the actorsList
+                actorsList.add(actor);
+            }
+
+            actorsListArray = actorsList.toArray(new String[actorsList.size()]); //finalize the actors list string
+            return Arrays.toString(actorsListArray);
+        }
+        else { //error message if no actor information is found
+            System.out.println("No actor information found.");
+        }
+        return Arrays.toString(actorsListArray);
     }
 }
